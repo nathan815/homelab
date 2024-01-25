dd3df1053136ac085d1f056a8e88d71456350f33
Nathan Johnson <nathancj224@gmail.com>
2024-01-25 02:48:06 -0500

    Add tags to each role in pi_setup.yml

--- ansible/run_pi_setup.sh
+++ ansible/run_pi_setup.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -it -v $PWD:/work -v ~/.ssh:/root/.ssh ansible ansible-playbook -e ansible_user=nathan -e @secrets.yml playbooks/pi_setup.yml
+docker run -it -v $PWD:/work -v ~/.ssh:/root/.ssh ansible ansible-playbook -e ansible_user=nathan -e @secrets.yml "$@" playbooks/pi_setup.yml
+
+# Examples
+# ./run_pi_setup.sh
+# ./run_pi_setup.sh --tags monitoring
+# ./run_pi_setup.sh --skip-tags base
