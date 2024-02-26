2585613c249f5e043ee191aced772a70f1a1b7dc
Nathan Johnson <nathancj224@gmail.com>
2024-02-25 20:55:51 -0500

    refactor playbooks - base, pi01, pi02, and custom runner script

---
+++ ansible/ansible_playbook.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+IMAGE=ansible:latest
+
+docker run -it -v $PWD:/work -v ~/.ssh:/root/.ssh "$IMAGE" \
+    ansible-playbook -e ansible_user=nathan -e @secrets.yml "$@"
+
+# Examples
+# ./run_playbook.sh pi_setup.yml
+# ./run_playbook.sh pi_setup.yml --tags monitoring
+# ./run_playbook.sh pi_setup.yml --skip-tags base
