d1252118df25864ca735262290b1742e94b0e8a3
Nathan Johnson <nathancj224@gmail.com>
2024-04-22 01:57:39 -0400

    ansible_playbook script updates

--- ansible/ansible_playbook.sh
+++ ansible/ansible_playbook.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+ARGS=$@
 
 IMAGE=ansible:latest
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 MOUNT_DIR="$(dirname "$SCRIPT_DIR")"
 
+CMD="cd /work/ansible && ansible-playbook -e @secrets.yml $ARGS"
+echo "[ansible container]  $CMD"
+
 docker run -it -v "$MOUNT_DIR":/work -v ~/.ssh:/root/.ssh "$IMAGE" \
-    sh -c "cd /work/ansible && ansible-playbook -e @secrets.yml $@"
+    sh -c "$CMD"
 
 # Examples
 # ./run_playbook.sh pi_setup.yml
