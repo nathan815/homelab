b9abc87bd228b8ced93aa547ebfcf8cf1e6dd993
Nathan Johnson <nathancj224@gmail.com>
2024-04-23 15:53:09 -0400

    nginx stack validate.sh reenable

--- stacks/nginx-proxy/validate.sh
+++ stacks/nginx-proxy/validate.sh
@@ -2,17 +2,17 @@ SCRIPT_DIR=$(dirname "$0")
 
 echo "--- Validating nginx_public.conf ---"
 
-# docker run --rm -v $SCRIPT_DIR/nginx_public.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
-# if [ $? -ne 0 ]; then
-#     echo "nginx_public.conf configuration is invalid"
-#     exit 1
-# fi
+docker run --rm -v $SCRIPT_DIR/nginx_public.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
+if [ $? -ne 0 ]; then
+    echo "nginx_public.conf configuration is invalid"
+    exit 1
+fi
 
-# echo ""
-# echo "--- Validating nginx_internal.conf ---"
+echo ""
+echo "--- Validating nginx_internal.conf ---"
 
-# docker run --rm -v $SCRIPT_DIR/nginx_internal.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
-# if [ $? -ne 0 ]; then
-#     echo "nginx_internal.conf configuration is invalid"
-#     exit 1
-# fi
+docker run --rm -v $SCRIPT_DIR/nginx_internal.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
+if [ $? -ne 0 ]; then
+    echo "nginx_internal.conf configuration is invalid"
+    exit 1
+fi
