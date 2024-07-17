7afc6bd81bf78acfa4bb506662c946ecd4faa2ea
Nathan Johnson <nathancj224@gmail.com>
2024-07-16 20:48:57 -0400

    nginx-proxy custom error page code

--- stacks/nginx-proxy/validate.sh
+++ stacks/nginx-proxy/validate.sh
@@ -1,8 +1,14 @@
 SCRIPT_DIR=$(dirname "$0")
 
+echo "SCRIPT_DIR = $SCRIPT_DIR"
+
 echo "--- Validating nginx_public.conf ---"
 
-docker run --rm -v $SCRIPT_DIR/nginx_public.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
+docker run --rm \
+    -v $SCRIPT_DIR/config/nginx_public.conf:/etc/nginx/conf.d/default.conf \
+    -v $SCRIPT_DIR/config/includes:/etc/nginx/conf.d/includes \
+    nginx nginx -t
+
 if [ $? -ne 0 ]; then
     echo "nginx_public.conf configuration is invalid"
     exit 1
@@ -11,7 +17,11 @@ fi
 echo ""
 echo "--- Validating nginx_internal.conf ---"
 
-docker run --rm -v $SCRIPT_DIR/nginx_internal.conf:/etc/nginx/conf.d/default.conf nginx nginx -t
+docker run --rm \
+    -v $SCRIPT_DIR/config/nginx_internal.conf:/etc/nginx/conf.d/default.conf \
+    -v $SCRIPT_DIR/config/includes:/etc/nginx/conf.d/includes \
+    nginx nginx -t
+
 if [ $? -ne 0 ]; then
     echo "nginx_internal.conf configuration is invalid"
     exit 1
